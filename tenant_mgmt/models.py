3c1f93835f2bde6fe0bd58821612790cbb1bd082
Brijesh Varsani <varsani340@gmail.com>
2022-05-26 01:53:28 +0200

    update

--- tenant_mgmt/models.py
+++ tenant_mgmt/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User, AbstractBaseUser
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=50, null=False)
+    apartment = models.CharField(max_length=10, null=False)
+    city = models.CharField(max_length=50, null=False)
+    zipCode = models.CharField(max_length=15, null=False)
+    country = models.CharField(max_length=50, null=False)  
+    dateAdded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.country} {self.zipCode} {self.city} , {self.street} {self.apartment}'
+
+    def get_absolute_url(self):
+        return reverse("addressDetails", kwargs={"pk": self.pk})
+    
+
+class Property(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True, related_name="address_properties_set")
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="owner_property_set", null=True, blank=True)
+    tenants = models.ManyToManyField(User, related_name="tenant_property_set", blank=True)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("propertyDetails", kwargs={"pk": self.pk})
+
+
+class IssueCategory(models.Model):
+    title = models.CharField(max_length=50)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("categories")
+
+
+class Issue(models.Model):
+    class StatusENUM(models.TextChoices):
+        CRE = 'CREATED',
+        ASS = 'ASSIGNED',
+        PRO = 'IN_PROGRESS',
+        COM = 'COMPLETED',
+        DEL = 'DELETED',
+        CLO = 'CLOSED'
+
+    title = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(max_length=200, null=False, blank=False)
+
+    category = models.ForeignKey(IssueCategory, on_delete=models.SET_NULL, null=True)
+    related_property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=StatusENUM.choices, default=StatusENUM.CRE)
+ 
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, default=None, blank=True, null=True, related_name="assigned_by_issues_set")
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, default=None, blank=True, null=True, related_name="assigned_to_issues_set")
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="submitted_issues_set")
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("issueDetails", kwargs={"pk": self.pk})
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+        
+
+class Comment(models.Model):
+    messageText = models.TextField(max_length=255, null=True)
+
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE, related_name="issue_comments")
+
+    dateAdded = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['dateUpdated', 'dateAdded']
+
+    def __str__(self):
+        return f'{self.author.username}: {self.messageText}'
--- tenant_mgmt/views.py
+++ tenant_mgmt/views.py
@@ -1,3 +1,254 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
+from .models import Address, Issue, Property, IssueCategory, Comment
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .decorators import admin_requred, manager_requred, no_employee_allowed
+from .forms import PropertyCreateForm, IssueCreateForm
+from . import util
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'tnt_mgmt/home.html')
+
+
+class CustomUpdateView(UpdateView):
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['update'] = True
+        return context
+
+
+@method_decorator([login_required], name='dispatch')
+class IssuesListView(ListView):
+    model = Issue
+    template_name = 'tnt_mgmt/issue/list.html'
+    context_object_name = 'issues'
+    paginate_by = 5
+
+    def get_queryset(self):
+        status = self.request.GET.get('status') 
+        if status:
+            if util.is_admin_or_manager(self.request.user):
+                return Issue.objects.filter(status=status).order_by('-created')
+            if util.is_employee(self.request.user):
+                 return Issue.objects.filter(status=status, assignee=self.request.user)
+        if util.is_admin_or_manager(self.request.user):
+            return Issue.objects.all()
+        if util.is_employee(self.request.user):
+            return Issue.objects.filter(assignee=self.request.user)
+        else:
+            return Issue.objects.filter(submitter=self.request.user)
+
+class IssuesForManagerListView(ListView):
+    model = Issue
+    template_name = 'tnt_mgmt/issue/list.html'
+    context_object_name = 'issues'
+    paginate_by = 5
+
+    def get_queryset(self):
+        status=self.kwargs.get('status')
+        print(Issue.StatusENUM.choices)
+        return Issue.objects.filter(status=Issue.StatusENUM.choices[status]).order_by('-created')
+        # if util.is_admin_or_manager(self.request.user):
+        #     return Issue.objects.all()
+        # if util.is_employee(self.request.user):
+        #     return Issue.objects.filter(assignee=self.request.user)
+        # else:
+        #     return Issue.objects.filter(submitter=self.request.user)
+
+
+@method_decorator([login_required], name='dispatch')
+class IssueDetailView(UserPassesTestMixin, DetailView):
+    model = Issue
+    template_name = 'tnt_mgmt/issue/detail.html'
+    context_object_name = 'issue'
+    ordering = ['dateAdded']
+
+    def get_context_data(self, **kwargs):
+        context = super(IssueDetailView, self).get_context_data(**kwargs)
+        context['employees'] = User.objects.filter(groups__name='employee')
+        return context
+
+    def post(self, request, *args, **kwargs):
+        issue = self.get_object()
+        if request.method == 'POST':
+            if request.POST['action'] == 'assign':
+                employee = User.objects.get(id=request.POST['assignee'])
+                issue.assignee = employee
+                issue.manager = request.user
+                issue.status = Issue.StatusENUM.ASS
+                issue.save()
+            elif request.POST['action'] == 'comment':
+                comment = Comment(
+                    messageText=request.POST['comment_text'], 
+                    issue=issue, 
+                    author=request.user
+                )
+                comment.save()
+            elif request.POST['action'] == 'setStatus':
+                issue.status = request.POST['status']
+                issue.save()
+        emps = User.objects.filter(groups__name='employee')
+        return render(request,self.template_name, {'issue':issue, 'employees': emps})
+
+    def test_func(self):
+        issue = self.get_object()
+        return util.is_admin_or_manager(self.request.user) or self.request.user == issue.submitter or self.request.user == issue.assignee
+
+
+@method_decorator([login_required], name='dispatch')
+class IssueUpdateView(UserPassesTestMixin, CustomUpdateView):
+    model = Issue
+    fields = ['title', 'description', 'category']
+    template_name = 'tnt_mgmt/issue/form.html'
+
+    def test_func(self):
+        issue = self.get_object()
+        return util.is_admin_or_manager(self.request.user) or self.request.user == issue.submitter
+
+@login_required
+@no_employee_allowed
+def newIssue(request):
+    if request.method == 'POST':
+        form = IssueCreateForm(request.user, request.POST)
+        if form.is_valid():
+            prop = form.save(commit=False)
+            prop.submitter = request.user
+            prop.save()
+            return redirect('issueDetails', pk = prop.id) 
+    else:
+        form = IssueCreateForm(user = request.user)
+    return render(request, 'tnt_mgmt/issue/form.html', {'form' : form })
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class PropertiesListView(ListView):
+    model = Property
+    template_name = 'tnt_mgmt/property/list.html'
+    context_object_name = 'properties'
+    paginate_by = 5
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class PropertyDetailView(DetailView):
+    model = Property
+    template_name = 'tnt_mgmt/property/detail.html'
+    context_object_name = 'property'
+    ordering = ['dateAdded']
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class PropertyUpdateView(CustomUpdateView):
+    model = Property
+    fields = ['name', 'owner', 'address']
+    template_name = 'tnt_mgmt/property/form.html'
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class AddressListView(ListView):
+    model = Address
+    template_name = 'tnt_mgmt/address/list.html'
+    context_object_name = 'addresses'
+    paginate_by = 5
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class AddressDetailView(DetailView):
+    model = Address
+    template_name = 'tnt_mgmt/address/detail.html'
+    context_object_name = 'address'
+    ordering = ['dateAdded']
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class AddressCreateView(CreateView):
+    model = Address
+    template_name = 'tnt_mgmt/address/form.html'
+    fields = ['country', 'city', 'zipCode', 'street', 'apartment']
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class AddressUpdateView(CustomUpdateView):
+    model = Address
+    fields = ['country', 'city', 'zipCode', 'street', 'apartment']
+    template_name = 'tnt_mgmt/address/form.html'
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class AddressDeleteView(DeleteView):
+    model = Address
+    template_name = 'tnt_mgmt/address/delete_confirm.html'
+    def get_success_url(self) -> str:
+        return reverse('addresses')
+
+
+@method_decorator([login_required, admin_requred], name='dispatch')
+class IssueCategoryDeleteView(DeleteView):
+    model = IssueCategory
+    template_name = 'tnt_mgmt/category/delete_confirm.html'
+
+    def get_success_url(self) -> str:
+        return reverse('categories')
+
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class PropertyDeleteView(DeleteView):
+    model = Property
+    template_name = 'tnt_mgmt/property/delete_confirm.html'
+    def get_success_url(self) -> str:
+        return reverse('addresses')
+
+@method_decorator([login_required, manager_requred], name='dispatch')
+class IssueDeleteView(DeleteView):
+    model = Issue
+    template_name = 'tnt_mgmt/issue/delete_confirm.html'
+    def get_success_url(self) -> str:
+        return reverse('issues')
+    
+
+@login_required
+@manager_requred
+def newProperty(request):
+    if request.method == 'POST':
+        form = PropertyCreateForm(request.POST)
+        if form.is_valid():
+            prop = form.save()
+            return redirect('propertyDetails', pk = prop.id) 
+    else:
+        form = PropertyCreateForm()
+    return render(request, 'tnt_mgmt/property/form.html', {'form' : form })
+
+@method_decorator([login_required, admin_requred], name='dispatch')
+class IssueCategoryListView(ListView):
+    model = IssueCategory
+    template_name = 'tnt_mgmt/category/list.html'
+    context_object_name = 'categories'
+    ordering = ['dateAdded']
+    paginate_by = 5
+
+@method_decorator([login_required, admin_requred], name='dispatch')
+class IssueCategoryCreateView(CreateView):
+    model = IssueCategory
+    template_name = 'tnt_mgmt/category/form.html'
+    fields = ['title']
+
+
+@method_decorator([login_required, admin_requred], name='dispatch')
+class IssueCategoryUpdateView(CustomUpdateView):
+    model = IssueCategory
+    fields = ['title']
+    template_name = 'tnt_mgmt/category/form.html'
+    
